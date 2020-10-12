d8341c7889e5c747f09d6ff75e8d0c1f9a1d0f30
Prabath Gamage <prabath.gamage@platform1.net>
2020-10-12 14:58:58 +0530

    v4.0.0

--- changelog.md
+++ changelog.md
@@ -1,4 +1,23 @@
-Version 1.0.0
+Version
+-------
+v1.0.0
+  v1.1.2 - removed CSS3 Pie
+  v1.1.3 - enabled control to be used in loops
+  v1.1.4 - fixed image loading glitch
+         - updated jquery to v1.11.1
+  v1.1.5 - added dk option
+  v1.1.6 - fixed dk page reload
+  v1.1.6 - fixed exclusive
+  v1.1.7 - fixed input text not shown when going back in the screen
+  v1.1.7 - added animatedResponses param
 
-- Feature 1
-- Feature 2
+v2.0.0 - theme implementation
+  v2.0.2 - Fixed visibility of the control
+  v2.0.3 - Fixed hover styling for small screen
+  v2.0.4 - Added compatibility with multiple
+  v2.0.5 - Added management of 2 exclusives responses
+  v2.0.6 - Fixed On by If
+
+v3.0.0 - AvailableAnswers
+
+v4.0.0 - Semi-opens
--- resources/dynamic/standard.js
+++ resources/dynamic/standard.js
@@ -1,5 +1,25 @@
 /* standard_default.js */
 $(window).load(function() {
+	{%
+	Dim i
+
+	Dim strOtherRID = ""
+	Dim strOtherQID = ""
+
+	Dim ar = CurrentQuestion.AvailableResponses
+	For i = 1 to ar.Count
+	    If ar[i].isOpen = True Then
+	        If strOtherRID <> "" Then
+	          strOtherRID = strOtherRID + ","
+	            strOtherQID = strOtherQID + ","
+	        Endif
+	        strOtherRID = strOtherRID + ar[i].Index
+	        strOtherQID = strOtherQID + ar[i].OpenQuestion.InputName()
+	    Endif
+	Next i
+
+	%}
+
 	$('#adc_{%= CurrentADC.InstanceId %}').adcRanking({
         instanceId : '{%= CurrentADC.InstanceId%}',
 		maxWidth : '{%= CurrentADC.PropValue("maxWidth") %}',
@@ -16,8 +36,13 @@ $(window).load(function() {
 		showResponseHoverBorder: {%= (CurrentADC.PropValue("showResponseHoverBorder") = "1") %},
 		showRankMoveControls: {%= (CurrentADC.PropValue("showRankMoveControls") = "1") %},
 		controlAlign : '{%= CurrentADC.PropValue("controlAlign") %}',
-		otherRID : '{%= CurrentADC.PropValue("otherRID") %}',
-		otherQID : '{%= CurrentADC.PropValue("otherQID") %}',
+		{% If (CurrentQuestion.Type = "numeric" ) Then %}
+			otherRID : '{%= CurrentADC.PropValue("otherRID") %}',
+			otherQID : '{%= CurrentADC.PropValue("otherQID") %}',
+		{% Else %}
+			otherRID : '{%= strOtherRID %}',
+			otherQID : '{%= strOtherQID %}',
+		{% EndIf %}
 		setMax : parseInt('{%= CurrentADC.PropValue("maxRankedItems") %}'),
 		dkActivated : {% If (CurrentQuestion.Type = "multiple") Then %}{% If (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive and CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count - 1].IsExclusive) Then %}2{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive) Then %}1{% Else %}0{% EndIf %}{% Else %}{% If (CurrentADC.PropValue("dkActivated") = "2") Then %}2{% ElseIf (CurrentADC.PropValue("dkActivated") = "1") Then %}1{% Else %}0{% EndIf %}{% EndIf %},
 		animatedResponses : {%= (CurrentADC.PropValue("animatedResponses") = "1") %},
